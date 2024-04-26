ad9df4c3c4f1bce26c721ee371d7271beeacb7d6
MinaAlmasi <mina.almasi00@gmail.com>
2024-04-26 13:35:01 +0200

    feat: plot over diff. scalings over diff. agent types (mean accuracy instead of cumulative)

--- src/plot_simulate.R
+++ src/plot_simulate.R
@@ -4,30 +4,18 @@ pacman::p_load(tidyverse, here)
 file_path <- here::here("data", "simulated_data.csv")
 simulated_data <- read.csv(file_path)
 
-# calculate cumulative accuracy for each agent in dataframe (denoted by agent number)
-
-
-# calculate cumulative accuracy within each agent number
+# calculate mean accuracy across agent type, trial and c
 grouped_data <- simulated_data %>%
-  group_by(agent_types, agent_number) %>% 
-  mutate(cumulative_accuracy = cumsum(accuracy)/1:n())  %>% 
-  ungroup()
-
-# take means
-grouped_data <- grouped_data %>%
-  group_by(agent_types, trial) %>%
-  summarise(mean_accuracy = mean(accuracy), mean_cumulative_accuracy = mean(cumulative_accuracy), sd_cum_acc = sd(cumulative_accuracy))
+  group_by(agent_types, trial, true_c) %>%
+  summarise(mean_accuracy = mean(accuracy), sd_accuracy = sd(accuracy))
 
 # plot the data
-plot <- ggplot(grouped_data, aes(x=trial, y=mean_cumulative_accuracy, color=agent_types)) +
-  geom_line() +
-  geom_point() +
-  labs(title="Simulated Data", x="Trial", y="Accuracy") +
-  # add shaded area for standard deviation
-  geom_ribbon(aes(ymin=mean_cumulative_accuracy-sd_cum_acc, ymax=mean_cumulative_accuracy+sd_cum_acc), alpha=0.2) +
+plot <- ggplot(grouped_data, aes(x=trial, y=mean_accuracy, color=agent_types)) +
+  geom_smooth() +
+  labs(title="Simulated Data", x="Trial", y="Mean Accuracy") +
 
   # facet wrap, put all plots side by side
-  facet_wrap(~agent_types) +
+  facet_wrap(~true_c) +
   
   # add lim to y
   ylim(0.20, 0.80) +
