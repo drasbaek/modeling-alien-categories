cdee525c6482f68411f97343c82c9548b96910f5
MinaAlmasi <mina.almasi00@gmail.com>
2024-05-01 11:11:50 +0200

    feat: add data input (currently not working)

--- src/fit_model.R
+++ src/fit_model.R
@@ -1,14 +1,18 @@
 # fit model script
 pacman::p_load(tidyverse, here, cmdstanr, rstan, posterior, brms)
 
-fit_model <- function(df, stan_filepath, ){
+fit_model <- function(df, stan_filepath){
     
 
     # prepare the data
     data <- list("ntrials" = length(df$trial),
-                 "nfeatures" = 
+                 "nfeatures" = 5, 
+                 "dangerous" = df$dangerous,
+                 "choices" = df$choices, 
+                 "stimuli" = df[,c("eyes", "legs", "spots", "arms", "color")]     
+                 "w_prior_values" = c(1, 1, 1, 1, 1), # consider changing
+                 "c_prior_values" = c(0, 1) # consider changing          
                                   )
-    
     # compile the model
     model <- cmdstan_model(stan_filepath, cpp_options = list(stan_threads = TRUE))
     
@@ -26,3 +30,16 @@ fit_model <- function(df, stan_filepath, ){
     
     return(samples)
 }
+
+# stan file path
+stan_filepath <- here::here("stan", "GCM.stan")
+
+# read data
+df <- read_csv(here::here("data", "simulated_data.csv"))
+
+# subset the data to just a single simulation
+df <- df %>% filter(agent_number == 1 & agent_types == "both_good")
+
+# fit model
+samples <- fit_model(df, here::here("stan", "GCM.stan"))
+
