b90f95498e24582f5eee4dbe4b6bb5bebc13a671
MinaAlmasi <mina.almasi00@gmail.com>
2024-05-05 14:20:43 +0200

    feat: add cumulative acc of participants

--- src/plot_data.R
+++ src/plot_data.R
@@ -3,11 +3,34 @@ pacman::p_load(tidyverse, here)
 # load txt
 df <- read_csv(here::here("data", "AlienData.txt"))
 
-# filter to only include session 1 (where eyes and spots indicate dangerous) and condition 2 (where task was solved alone)
-df <- df %>% filter(session == 1 & condition == 2)
+# filter to only include session 1 (where eyes and spots indicate dangerous), condition 1 (where task was solved alone) and 96 trials (ignoring the stimulus that has pt at the end of their feature string)
+df <- df %>% filter(session == 1 & condition == 2 & trial <= 96)
 
-df
+# add a column to indicate whether responses were correct on dangerous condition
+df$dangerous_accuracy <- ifelse(df$dangerous == 1 & df$response %in% c(2, 4) | df$dangerous == 0 & df$response %in% c(1, 3), 1, 0)
 
-print(colnames(df))
+df$subject
 
-print(df)
+cumsum_df <- df %>%
+  group_by(subject) %>% 
+  mutate(cumulative_dangerous_accuracy = cumsum(dangerous_accuracy)/1:n())  %>% 
+  ungroup() 
+  
+
+grouped_df <- cumsum_df %>%
+    group_by(trial) %>%
+    summarise(mean_dangerous_accuracy = mean(cumulative_dangerous_accuracy),
+              sd_dangerous_accuracy = sd(cumulative_dangerous_accuracy)) 
+
+# do the plot
+plot <- ggplot() +
+  geom_hline(yintercept=0.5, linetype="dashed", color = "black") +
+  geom_line(data=cumsum_df, aes(x=trial, y=cumulative_dangerous_accuracy, group = subject), alpha=0.1) +
+  geom_smooth(data=cumsum_df, aes(x=trial, y=cumulative_dangerous_accuracy), se=FALSE, color="#0f5bea") +
+  geom_ribbon(data=grouped_df, aes(x=trial, y=mean_dangerous_accuracy, ymin = mean_dangerous_accuracy - sd_dangerous_accuracy, ymax = mean_dangerous_accuracy + sd_dangerous_accuracy), alpha=0.1) +
+  labs(x="Trial", y="Cumulative Accuracy") +
+  coord_cartesian(xlim = c(1, 96), ylim = c(0.20, 0.80)) +
+  theme_bw()
+
+# save
+ggsave(here::here("plots", "alien_data_cumulative.png"), plot, width=10, height=6, units="in", dpi=300)
