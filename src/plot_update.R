026688244e0da508ddef27370ec8c9bc46b3b971
MinaAlmasi <mina.almasi00@gmail.com>
2024-05-05 11:13:27 +0200

    feat: update plot with new pretty labels, nested factes (including c) and prior

--- src/plot_update.R
+++ src/plot_update.R
@@ -1,4 +1,4 @@
-pacman::p_load(tidyverse, here, cmdstanr, posterior, ggpubr, ggridges)
+pacman::p_load(tidyverse, here, cmdstanr, posterior, ggpubr, ggridges, ggh4x)
 
 plot_prior_posterior_update <- function(samples, params){
     
@@ -33,28 +33,64 @@ for (type in selected_types){
     }
 }
 
+# PLOTTING WEIGHTS # 
+weight_df <- final_df[, c("agent_type", "true_c", "w[1]", "w[2]", "w[3]", "w[4]", "w[5]", "w_prior[1]", "w_prior[2]", "w_prior[3]", "w_prior[4]", "w_prior[5]")]
 
-subset_df <- final_df[, c("agent_type", "true_c", "w[1]", "w[2]", "w[3]", "w[4]", "w[5]")] 
-subset_df <- subset_df %>% 
-    pivot_longer(cols=c("w[1]", "w[2]", "w[3]", "w[4]", "w[5]"), names_to = "w_number", values_to = "w_value") 
+weight_df <- weight_df %>% 
+    pivot_longer(cols=c("w[1]", "w[2]", "w[3]", "w[4]", "w[5]", "w_prior[1]", "w_prior[2]", "w_prior[3]", "w_prior[4]", "w_prior[5]"), names_to = "w_number", values_to = "w_value") 
 
-subset_df$true_c <- as.factor(subset_df$true_c)
+# split w_number col 
+weight_df$distribution <- ifelse(grepl("prior", weight_df$w_number), "prior", "posterior")
+weight_df$w_number <- gsub("_prior", "", weight_df$w_number)
 
-# plot with geom ridge
-plot <- ggplot(subset_df, aes(x = w_value, y = w_number, fill = w_number)) +
-    geom_density_ridges(scale = 0.9, rel_min_height = 0.01) +
-    theme_minimal() +
-    labs(x = "Weight Value") +
-    theme(legend.position = "top") +
-    scale_fill_manual(values = c("w[1]" = "#9758C3", "w[2]" = "#E357AC", "w[3]" = "#FF6D8A", "w[4]" = "#FF986B", "w[5]" = "#FFC95C")) +
-    facet_grid(~agent_type) + 
-    # custom legend title
-    guides(fill = guide_legend(title = "Weight Parameter")) +    
-    # reduce white space between y axis and first dist
+# ensure that distribution is a factor with prior as the first level
+weight_df$distribution <- factor(weight_df$distribution, levels = c("prior", "posterior"))
+
+weight_df$true_c <- as.factor(weight_df$true_c)
+
+# change factor levels for agent type 
+weight_df$agent_type <- factor(weight_df$agent_type, levels = c("both_good", "one_good", "neutral"))
+
+# add the true weights (based on actual values used in simulate.R)
+weight_df <- weight_df %>%
+  mutate(true_w = case_when(
+    (w_number == "w[1]" | w_number == "w[3]") & agent_type == "both_good" ~ 0.5,
+    w_number %in% c("w[2]", "w[4]", "w[5]") & agent_type == "both_good" ~ 0,
+    w_number == "w[1]" & agent_type == "one_good" ~ 0.5,
+    w_number %in% c("w[2]", "w[3]", "w[4]", "w[5]") & agent_type == "one_good" ~ 0.125,
+    agent_type == "neutral" ~ 0.2,
+    TRUE ~ NA_real_  # For handling NA if there was
+  ))
+
+# set facet labels
+agent_type_labs <- c("Eyes-Spots Sensitive", "Eyes Sensitive", "Ignorant")
+names(agent_type_labs) <- c("both_good", "one_good", "neutral")
+
+weight_labs <- c("Eyes", "Legs", "Spots", "Arms", "Color")
+names(weight_labs) <- c("w[1]", "w[2]", "w[3]", "w[4]", "w[5]")
+
+scaling_labs <- c("Low Scaling", "High Scaling")
+names(scaling_labs) <- c("0.3", "5")
+
+plot <- ggplot(weight_df, aes(w_value, fill=distribution)) +
+    geom_density(alpha=0.6) +
+    labs(x = "Weight Value", fill = "Distribution") +
+    # add true val 
+    geom_vline(aes(xintercept = true_w), linetype = "dashed", color = "black") +
+    # legend values and labels
+    scale_fill_manual(values = c("prior" = "lightgrey", "posterior" = "#0f5bea"), labels=c("Prior", "Posterior")) +
+    # do all facets 
+    facet_nested(agent_type + true_c ~ w_number, switch="y", labeller = labeller(agent_type=agent_type_labs, w_number=weight_labs, true_c=scaling_labs)) + 
+    # scale y 
     scale_y_discrete(expand = c(0, 0.10)) +
+    # add theme + custom theme elements
     theme_bw() + 
-    # remove y axis title and axis text
-    theme(axis.title.y = element_blank(), axis.text.y = element_blank()) 
+    theme(axis.title.y = element_blank(), axis.text.y = element_blank(), 
+          panel.spacing = unit(1, "lines"), strip.text = element_text(size = 13, face="bold"), strip.background = element_rect(fill="lightgrey"),
+          legend.text = element_text(size = 14), legend.title = element_text(size = 14), legend.position = "bottom",
+          axis.title.x = element_text(size = 14), axis.title = element_text(size = 14))
 
 # save plot 
 ggsave(here::here("plots", "weights_prior_posterior_update.png"), plot, width = 10, height = 10, units = "in", dpi = 300)
+
+subset_df
